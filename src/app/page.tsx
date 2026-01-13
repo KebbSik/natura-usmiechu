3e1ced15727aad9d2d24add0c1c1f03f96b94a5b
Kevin <kevinn.sarfoo1@gmail.com>
2026-01-13 21:42:54 +0100

    Added diveder to hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,34 @@ import Hero from "./components/Hero";
 import Testimonials from "./components/Testimonail";
 import Details from "./components/Details";
 import Banner from "./components/Banner";
-import Connector from "./svg/connector.svg";
+
 export default function Home() {
   return (
     <>
       <Hero />
-
+      <Image
+        src="/svg/connector.svg"
+        alt=""
+        width={400}
+        height={40}
+        className="w-full absolute -translate-y-1/2 "
+      />
       <Testimonials />
       <Image
         src="/svg/connector.svg"
         alt=""
         width={400}
         height={40}
-        className="w-full opacity-40"
+        className="w-full absolute -translate-y-1/2 "
       />
       <Details />
+      <Image
+        src="/svg/connector.svg"
+        alt=""
+        width={400}
+        height={40}
+        className="hidden md:block md:w-full md:absolute md:-translate-y-1/2 "
+      />
       <Banner />
     </>
   );
